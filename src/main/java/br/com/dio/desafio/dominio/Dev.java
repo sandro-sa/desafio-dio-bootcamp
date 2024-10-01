2db91619b221250e2b519f7b547d63b9dcb6fb74
sandro-sa <sandrosa0315@outlook.com>
2024-10-01 15:48:25 -0300

    Concluindo a criação das classe do domínio

--- src/main/java/br/com/dio/desafio/dominio/Dev.java
+++ src/main/java/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,9 +11,28 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void isnscreverBootcamp(Bootcamp bootcamp){}
-    public void progredir(){}
-    public void calcularTotalXp(){}
+    public void isnscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
+
+    public void progredir(){
+        //Optional resolve problemas de conteúdos nulos
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+            System.err.println("Você não esta matricúlado em nenhum conteúdo.");
+        }
+    }
+
+    public double calcularTotalXp(){
+        //return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
+        return this.conteudosConcluidos.stream()
+                                        .mapToDouble(Conteudo::calcularXp)
+                                        .sum();
+    }
 
     public String getNome() {
         return nome;
