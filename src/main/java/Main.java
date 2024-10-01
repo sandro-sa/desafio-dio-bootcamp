081adb05e21159ef3481236d6e1756d67311d003
sandro-sa <sandrosa0315@outlook.com>
2024-10-01 16:25:01 -0300

    Interligando as classee dominios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,8 +23,39 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do bootcamp java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        System.out.println();
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.isnscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos João"+devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+
+        System.out.println("-----");
+        System.out.println("Conteúdos inscritos João"+devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos conluidos João"+devJoao.getConteudosConcluidos());
+        System.out.println("XP: "+devJoao.calcularTotalXp());
+        System.out.println();
+
+        Dev devThereza = new Dev();
+        devThereza.setNome("Thereza");
+        devThereza.isnscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Thereza"+devThereza.getConteudosInscritos());
+        devThereza.progredir();
+        System.out.println("-----");
+        System.out.println("Conteúdos inscritos Thereza"+devThereza.getConteudosInscritos());
+        System.out.println("Conteúdos concluidos Thereza"+devThereza.getConteudosConcluidos());
+        System.out.println("XP: "+devThereza.calcularTotalXp());
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
     }
 }
